2085b90a1867d9454b43c12a669c96f93732dee2
Ankan Ghosh <ankanprs9@gmail.com>
2025-01-31 16:39:45 +0000

    datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,49 @@
+//premtive
+
+// 7 TYPES : string,number,boolean,BigInt,Symbol,null,undefined,
+
+const outsideTemp = null
+let userEmail;
+
+console.log(outsideTemp);
+console.log(userEmail);
+const BigNumber=14n //  FOR BIG INT TAKE n
+
+console.log(typeof BigNumber)
+
+//Non-premtive (REFERENCE)
+
+//Arrays , objects , functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"]; //ARRAYS 
+
+
+let myObj = {
+    name: "hitesh",         // OBJECT
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");          //FUNCTION
+}
+
+console.log(typeof heros)
+console.log(typeof myFunction)
+
+
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+Number => number
+String  => string
+Boolean  => boolean
+null  => object
+undefined  =>  undefined
+Symbol  =>  symbol
+BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+Arrays  =>  object
+Function  =>  function
+Object  =>  object*/
+
